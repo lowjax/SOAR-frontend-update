0f7bad303d57a16861394cd6a1428be83e036d07
Jack Cooke <jack.wheet.cooke@gmail.com>
2022-05-30 18:50:54 +1000

    user aray fixed, delete in progress

--- src/components/AdminUserCRUD.jsx
+++ src/components/AdminUserCRUD.jsx
@@ -1,29 +1,76 @@
 import React from "react"
 import NavbarAdmin from "./NavbarAdmin"
 import SoarLogo from "../images/SoarLogo.svg"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, resolvePath } from "react-router-dom"
 import { useState, useEffect} from "react"
 import { Spinner } from "react-bootstrap"
 import axios from "axios"
 
-export default function UserCRUD() {
+export default function UserCRUD(params) {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
     const [User, setUser] = useState([])
+    
+    const [email, setEmail] = useState({})
+    let data=({email:email})
+    
 
     useEffect(() => {
-        axios
-           .get("http://localhost:1235/api/users")
-           .then((response) => {
-              console.log(response)
-              // console.log(1, response.data[0])
-              setUser(response.data)
+           fetch("http://localhost:1235/api/users", {
+            credentials: "include",
+           })
+           .then((res) => res.json())
+           .then((json) => {
+              console.log(json)
+              setUser(json)
            })
            .catch(function (error) {
               // handle error
               console.log(error)
            })
+
+          
      }, [])
+     
+     var myHeaders = new Headers()
+      myHeaders.append("Content-Type", "application/json")
+
+      // var requestOptions = {
+      //    method: 'delete',
+      //    // // headers: myHeaders,
+      //    body: value,
+      //    // // redirect: 'follow'
+      //    // credentials: "include",
+      // }
+function deleteUser(params) {
+   var requestOptions = {
+      method: 'DELETE',
+      // // headers: myHeaders,
+      body: params.rows.email,
+      // // redirect: 'follow'
+      // credentials: "include",
+   }
+     fetch("http://localhost:1235/api/users/delete", + requestOptions)
+     console.log(requestOptions, data)
+     .then((response) => {
+      console.log(response)
+      if (response.status == 200) {
+         console.log(response)
+
+         alert("Success! You have deleted a user")
+         // setOpenModal(true)
+         window.location.href = "AdminUserCRUD"
+         return
+      }
+   })
+   .catch((e) => {
+      // console.log(bodyContent);
+      console.log(e)
+      alert("Sorry, something isn't right")
+      //return;
+   })
+   
+}
      
 
     return (
@@ -42,7 +89,13 @@ export default function UserCRUD() {
                            <p className="card-text">{users.date_joined}</p>
                            <p className="card-text">{users.user_status}</p>
                            <button className="btn btn-primary" id="favoritesButton" type="button">
-                              Delete User
+                              UPDATE
+                           </button>
+                           <button onClick={(params) => {
+                              setEmail(params.rows.email);deleteUser()
+                              }}>
+
+                              DELETE
                            </button>
                         </div>
                      </div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -125,7 +125,7 @@ export default function Login() {
          })
    }
    //
-   useEffect(() => {}, [])
+   // useEffect(() => {}, [])
 
    return (
       <div>
